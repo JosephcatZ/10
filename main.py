f945dd525578f6a4650f639f3b413f5c927f0e6f
Joseph catZ <legojosephcatz@gmail.com>
2019-11-17 20:31:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+Input = "1113222113"
+def looknsay(x):
+    x = str(x)+" "
+    total = ""
+    temp = ""
+    num = 0
+    for i in x:
+        if i == temp:
+            num+=1
+        else:
+            if temp != "":
+                total = total + str(num) + temp
+            temp = i
+            num = 1
+    return(total)
+o = Input
+for i in range(40):
+    print(i)
+    o = looknsay(o)
+print("Part 1:",o)
